f02eb24896bd4724092aa4560f4d4f484f8e85e5
Nick <nickho0988@gmail.com>
2026-08-29 20:34:43 +0700

    Add files via upload

---
+++ BerkasInternal/config/database.php
@@ -0,0 +1,18 @@
+<?php 
+    class database {
+        private $host ="localhost";
+        private $username = "root";
+        private $password = "";
+        private $database = "e-berkas";
+        public $conn;
+
+    public function __construct(){
+        $this->conn = mysqli_connect(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->database
+            );
+    }
+}
+?>
